93a45d0cae38d707361822b16ac62f1a42820aa2
James Cook <james@jaminit.co.uk>
2020-10-11 10:07:17 +0100

    Load allowed hosts from .env

--- FileSharing/settings.py
+++ FileSharing/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = os.get("ALLOWED_HOSTS")
 
 
 # Application definition
