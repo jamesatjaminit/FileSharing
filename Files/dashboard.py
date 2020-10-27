da359a467aa15542dd0cde2999b04237ecd34e94
James Cook <james@jaminit.co.uk>
2020-10-27 12:37:05 +0000

    Clean up files

--- Files/dashboard.py
+++ Files/dashboard.py
@@ -10,11 +10,11 @@ from Files.models import File
 def main(request):
     if request.user.is_authenticated: # Makes sure they are logged in
         result = File.objects.filter(belongsto=request.user.id) # Gets all the files owned by the user id
-        paginator = Paginator(result, 3) # Paginates
+        paginator = Paginator(result, 3) # Paginates by 3
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         # result[0].description
         return render(request, "dashboard.html",
-                      {"entries": result, 'page_obj': page_obj, "hostname": os.getenv("HOSTNAME"), "request": request}) # Renders the template
+                      {"entries": result, 'page_obj': page_obj, "hostname": os.getenv("HOSTNAME"), "request": request}) # Renders the admin dashboard
     else:
         return HttpResponseRedirect(os.getenv("HOSTNAME") + "/files/login?redirect=files/dashboard&errorcode=0") # Redirects back to login page as they aren't logged in
--- Files/renderfile.py
+++ Files/renderfile.py
@@ -7,15 +7,25 @@ from django.shortcuts import render
 from Files.models import File
 
 
-def error404(request):
+def error404(request): # Returns 404 page
     return render(request, "404.html", {"hostname": os.getenv("HOSTNAME"), "request": request})
+
+
+def renderText(filename, request, errormessage):
+    try:
+        text = open(os.getenv("BASE_PATH") + r"Files\Uploads\\" + filename)
+        return render(request, "rendertext.html",
+                      {"text": text.read(), "hostname": os.getenv("HOSTNAME"), "request": request,
+                       "errormessage": errormessage})
+    except FileNotFoundError:
+        return error404(request)
 
 
 def renderFile(request, filename):
     file = File.objects.filter(name=filename)  # Gets the file from the db
-    if len(file) == 0:
+    if len(file) == 0: # Checks if 0 entries were returned (file isn't in db)
         return error404(request)
-    errorCode = request.GET.get('errorcode')  # Gets the error code if any
+    errorCode = request.GET.get('errorcode')  # Gets the error code (if any)
     if errorCode == "1":  # Happens when user is authenticated but wants a private paste
         errormessage = "You asked us to make a private paste, but since you're not logged in, we had to make it " \
                        "unlisted "
@@ -26,26 +36,17 @@ def renderFile(request, filename):
             if request.user.is_authenticated:  # Check if they are logged into and account
                 if file[0].belongsto == request.user.id or request.user.is_staff:  # Checks if the user is the owner of the file OR staff
                     if not file[0].belongsto == request.user.id and request.user.is_staff:  # If the it doesn't belong to the user but they are staff
-                        viewBecauseStaff = True  # Set to true
+                        viewBecauseStaff = True
                     else:
                         viewBecauseStaff = False
-                    text = open(os.getenv("BASE_PATH") + r"Files\Uploads\\" + filename)  # Opens the file
-                    try:
-                        text = open(os.getenv("BASE_PATH") + r"Files\Uploads\\" + filename)
-                        return render(request, "rendertext.html",
-                                      {"text": text.read(), "hostname": os.getenv("HOSTNAME"), "request": request,
-                                       "errormessage": errormessage})
-                    except FileNotFoundError:
-                        return error404(request)
+                    return renderText(filename, request, errormessage)
                 else:
                     return render(request, "forbidden.html", status=403)  # Redirect user as they don't have access
             else:
                 return (HttpResponseRedirect(os.getenv(
                     "HOSTNAME") + "/files/login?redirect=files/f/" + filename + "&errorcode=1"))  # User isn't logged in, redirect them to the login page
-    try:
-        text = open(os.getenv("BASE_PATH") + r"Files\Uploads\\" + filename)
-        return render(request, "rendertext.html",
-                      {"text": text.read(), "hostname": os.getenv("HOSTNAME"), "request": request,
-                       "errormessage": errormessage})
-    except FileNotFoundError:
-        return error404(request)
+    elif file[0].visibility == 'unlisted': # Unlisted file, anyone can access with link
+        return renderText(filename, request, errormessage)
+    else: # If the visibility for some reason is different simply 404 error and log the error
+        print('Visibility not unlisted or private')
+        error404(request)
